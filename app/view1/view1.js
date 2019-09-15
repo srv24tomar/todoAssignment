368606236491521f5601e7a4071630775579e64f
srv24tomar <tomsrv92@gmail.com>
2019-09-15 14:29:59 +0530

    Update view1.js

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,15 +9,19 @@ angular.module("myApp.view1", ["ngRoute"])
   });
 }])
 
-.controller("view1Ctrl", function($scope, $timeout) {
+.controller("view1Ctrl", function($scope, $http, $timeout) {
 
   $scope.selectFolder = function(fId, tData){
     if(fId !== null) {
       $scope.selectedFolder = fId;
       $scope.listToDos = [];
-      angular.forEach($scope.listData.listFolder, function(val, key){
+      angular.forEach($scope.listData, function(val, key){
         if(val.folderId === fId) {
-          $scope.listToDos.push(val.folderItem);
+          $scope.listToDos = val.folderItem;
+          angular.forEach($scope.listToDos, function(v, k){
+            v.listCreatedAt = new Date(v.listCreatedAt).getTime();
+          });
+          //new Date('8-23-2019 14:35:41').getTime()
           $scope.showTextArea = false;
           $scope.toDoItem = '';
         }
@@ -49,32 +53,31 @@ angular.module("myApp.view1", ["ngRoute"])
     }
   };
 
-  $scope.addNewNote = function(folder, note){
-    if(folder){
+  $scope.addNewNote = function(fName, note){
+    if(fName){
       var newFolder = {
-        "folderName" : folder,
-          "folderId" : "f3",
+        "folderName" : fName,
+          "folderId" : "f"+Math.floor(Math.random() * 10),
           "folderItem" : []
       }
-      $scope.listData.listFolder.push(newFolder);
-      $scope.newFolder = '';
+      $scope.listData.push(newFolder);
+      document.getElementsByClassName('newFolderName')[0].value = '';
     }else {
-      if(!angular.isUndefined(note)){
+      if(!angular.isUndefined(note) && note.length > 0 ){
         var newNote = {
-          "listId" : "f3l1",
+          "listId" : 'l'+Math.floor(Math.random() * 10)+'_'+Math.floor(Math.random() * 10),
           "listContent" : note,
           "listCreatedAt" : Date.now()
         }
-        angular.forEach($scope.listData.listFolder, function(val, key){
+        angular.forEach($scope.listData, function(val, key){
           if(val.folderId === $scope.selectedFolder){
-            $scope.listData.listFolder[key].folderItem.push(newNote);
+            $scope.listData[key].folderItem.push(newNote);
           }
         });
         $scope.newNote = '';
       }else{
         alert('add note');
       }
-      
     }
   };
 
@@ -82,6 +85,7 @@ angular.module("myApp.view1", ["ngRoute"])
     $scope.showTextArea = false;
     $scope.showDelete = true;
     $scope.toDoItem.listContent = $scope.editedNote;
+    $scope.toDoItem.listCreatedAt = Date.now();
   };
 
   $scope.editNote = function(edit, note){
@@ -103,68 +107,30 @@ angular.module("myApp.view1", ["ngRoute"])
   $scope.deleteNote = function(del){
     $scope.toDoItem = '';
     $scope.showDelete = false;
-    angular.forEach($scope.listData.listFolder, function(val, key){
+    angular.forEach($scope.listData, function(val, key){
       if(val.folderId === $scope.selectedFolder){
         angular.forEach(val.folderItem, function(v,k){
           if(del.listId === v.listId){
-            $scope.listData.listFolder[key].folderItem.splice(k, 1);
+            $scope.listData[key].folderItem.splice(k, 1);
           }
         });
       }
+    });
+  };
+
+  $scope.getToDoData = function(){
+    $http.get("https://my-json-server.typicode.com/srv24tomar/myFakeJsonServer/listFolder").then(function(response) {
+      $scope.listData = response.data;
+      console.log($scope.listData);
     });
   };
   
   $scope.init = function() {
     $scope.startTimer = true;
     $scope.toDoItem = '';
-    $scope.listData = {
-      "listFolder":[
-        {
-          "folderName" : "folder a",
-          "folderId" : "f1",
-          "folderItem" : [
-            {
-              "listId" : "f1l1",
-              "listContent" : "Show on Nat Geo",
-              "listCreatedAt" : "13:35:41"
-            },
-            {
-              "listId" : "f1l2",
-              "listContent" : "Pickup friend from airport at 8AM",
-              "listCreatedAt" : "12:35:41"
-            },
-            {
-              "listId" : "f1l3",
-              "listContent" : "Office at 11AM",
-              "listCreatedAt" : "10:35:41"
-            }
-          ]
-        },
-        {
-          "folderName" : "folder b",
-          "folderId" : "f2",
-          "folderItem" : [
-            {
-              "listId" : "f2l1",
-              "listContent" : "I have an interview at 10 AM on saturday",
-              "listCreatedAt" : "19:35:41"
-            },
-            {
-              "listId" : "f2l2",
-              "listContent" : "Need to finish assignment by Sunday",
-              "listCreatedAt" : "18:35:41"
-            },
-            {
-              "listId" : "f2l3",
-              "listContent" : "Zoo will be closed",
-              "listCreatedAt" : "17:35:41"
-            }
-          ]
-        }
-      ]
-    };
-};
+    $scope.getToDoData();
+  };
 
-$scope.init();
+  $scope.init();
 
 });
